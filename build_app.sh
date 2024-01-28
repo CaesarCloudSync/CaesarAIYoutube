1415708bc8434d93791c32cc22bb15d17010a3c8
amari <amari.lawal@gmail.com>
2024-01-28 20:38:22 +0000

    caesarai youtube works

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
-git push origin 
+git push origin -u main:main
 docker build -t palondomus/caesaraiyoutube:latest .
 docker push palondomus/caesaraiyoutube:latest
 docker run -it -p 8080:8080 palondomus/caesaraiyoutube:latest
