805ae5ce264dbf2d51ac04167c47799a154cb62a
amari <amari.lawal@gmail.com>
2024-01-28 22:36:29 +0000

    also shows the content length so download time can be displayed.

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin -u main:main
-docker build -t palondomus/caesaraiyoutube:latest .
-docker push palondomus/caesaraiyoutube:latest
-docker run -it -p 8080:8080 palondomus/caesaraiyoutube:latest
+docker build -t palondomus/caesaraiyoutube:finest .
+docker push palondomus/caesaraiyoutube:finest
+docker run -it -p 8080:8080 palondomus/caesaraiyoutube:finest
