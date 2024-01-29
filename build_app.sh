ddfbbd7d9b92719c762ef785300906e77fd20849
amari <amari.lawal@gmail.com>
2024-01-29 19:16:12 +0000

    no bs4

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin -u main:main
-docker build -t palondomus/caesaraiyoutube:newest .
-docker push palondomus/caesaraiyoutube:newest
-docker run -it -p 8080:8080 palondomus/caesaraiyoutube:newest
+docker build -t palondomus/caesaraiyoutube:coolest .
+docker push palondomus/caesaraiyoutube:coolest
+docker run -it -p 8080:8080 palondomus/caesaraiyoutube:coolest
