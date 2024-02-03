955ae7028bb3693eaefe4ab44dd1051d911c0280
amari <amari.lawal@gmail.com>
2024-02-03 01:16:01 +0000

    youtube works using google buckets

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin -u main:main
-docker build -t palondomus/caesaraiyoutube:coolest .
-docker push palondomus/caesaraiyoutube:coolest
-docker run -it -p 8080:8080 palondomus/caesaraiyoutube:coolest
+docker build -t palondomus/caesaraiyoutube:latest .
+docker push palondomus/caesaraiyoutube:latest
+docker run -it -p 8080:8080 palondomus/caesaraiyoutube:latest
