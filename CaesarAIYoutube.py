68d6028e9de415cc00865351f26a38b7ed22928a
amari <amari.lawal@gmail.com>
2024-02-05 18:33:19 +0000

    CaesarAI StreamUpload GCP

--- CaesarAIYoutube.py
+++ CaesarAIYoutube.py
@@ -1,19 +1,13 @@
-import io
 import re
-import json
-import base64
-from google.cloud import storage
+import sys
+from CaesarAIGCP.CaesarAIGCP import CaesarAIGCPStreamUpload
 from pytube import YouTube,request
 from youtubesearchpython import VideosSearch,PlaylistsSearch,Playlist
 
 
 class CaesarAIYoutube:
     def __init__(self) -> None:
-        with open("creds.txt") as f:
-            service_base64 = f.read()
-        service_info = json.loads(base64.b64decode(service_base64.encode()).decode())
-        self.storage_client = storage.Client.from_service_account_info(service_info)
-
+        pass
     def searchfeed(self,query:str,amount=10):
         videosSearch = VideosSearch(query, limit =amount)
 
@@ -38,6 +32,7 @@ class CaesarAIYoutube:
     def get_video(self,url):
         yt = YouTube(url)
         video = yt.streams.filter(progressive=True).get_highest_resolution()
+        video.filesize
         if video:
             return video
         else:
@@ -45,52 +40,18 @@ class CaesarAIYoutube:
     def get_playlist_videos(self,url):
         playlist = Playlist(url)
         return {"result":playlist.videos}
-    def make_blob_public(self,blob_name,bucket_name:str="caesaraiyoutube-bucket"):
-        """Makes a blob publicly accessible."""
-        # bucket_name = "your-bucket-name"
-        # blob_name = "your-object-name"
-        bucket = self.storage_client.bucket(bucket_name)
-        blob = bucket.blob(blob_name)
-
-        blob.make_public()
-
-        #print(
-        #    f"Blob {blob.name} is publicly accessible at {blob.public_url}"
-        #)
-
-    def upload_to_bucket(self,  file_bytes,blob_name:str,bucket_name:str="caesaraiyoutube-bucket"):
-        """ Upload data to a bucket"""
-        
-        # Explicitly use service account credentials by specifying the private key
-        # file.
-
-
-        #print(buckets = list(storage_client.list_buckets())
-        bucket = self.storage_client.get_bucket(bucket_name)
-        blob = bucket.blob(blob_name)
-        blob.upload_from_file(file_bytes) #upload_from_filename(path_to_file)
-        
-        #returns a public url
-        self.make_blob_public(blob_name,bucket_name)
-        return blob.public_url
-    def get_all_media(self,bucket_name:str="caesaraiyoutube-bucket"):
-        bucket =self.storage_client.get_bucket(bucket_name)
-        blobs = bucket.list_blobs()
-        return [{"title":blob.name,"url":blob.media_link}for blob in blobs]
-
-
-    def delete_all_media(self,bucket_name:str="caesaraiyoutube-bucket"):
-        bucket =self.storage_client.get_bucket(bucket_name)
-        blobs = bucket.list_blobs()
+    def stream_to_bucket(self,mediaurl:str,filesize:str,bucket_name:str,blob_name:str):
+        with CaesarAIGCPStreamUpload(bucket_name=bucket_name, blob_name=blob_name) as s:
+            for ind,chunk in enumerate(request.stream(mediaurl)):
     
-        for blob in blobs:
-            print(blob)
-            blob.delete()
+                s.write(chunk)
+                yield f"{(ind * sys.getsizeof(chunk)/filesize) * 100}%"
+                
 
 
 if __name__ == "__main__":
     cy = CaesarAIYoutube()
-    print(cy.get_all_media())
+    #print(cy.get_all_media())
     #caesaryoutube = CaesarAIYoutube()
     
 
